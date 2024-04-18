9cec86a2f696cf3cdca96f676de7b0034f478697
t-nician <dev@senpai.zone>
2024-04-18 19:12:44 -0400

    Some slight cleaning.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,8 @@
-﻿using Conway;
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-Game game = new Game(30, 30);
+﻿Conway.Game game = new Conway.Game(30, 50);
 
 
 while (true) {
+    Console.Clear();
     game.Output();
     game.Step();
 
