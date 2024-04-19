8acd33f4d129fd8100cb5424b51c6511da7f342d
t-nician <dev@senpai.zone>
2024-04-18 20:08:44 -0400

    Yay more input!

--- Program.cs
+++ Program.cs
@@ -18,7 +18,12 @@ var demo_options = new List<string>() {
 while (true) {
     switch (Inputter.Ask.Choice("Choose Demonstration", demo_options)) {
         case "conwaysgameoflife":
-            new Conway.Game(10, 30).Loop(Inputter.Ask.IntMinMax("Input delay(miliseconds) for each game update.", 30, 1000));
+            new Conway.Game(
+                Inputter.Ask.IntMinMax("Provide game width.", 5, 10), 
+                Inputter.Ask.IntMinMax("Provide game height.", 10, 30)
+            ).Loop(
+                Inputter.Ask.IntMinMax("Input delay(miliseconds) for each game update.", 30, 1000)
+            );
             break;
 
         
